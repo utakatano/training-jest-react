088399f8fb2843cb53f6e7d5fbe1809e511b9722
kei-yagi <kei.yagi@gmail.com>
2022-02-22 23:54:59 +0900

    matchers

--- app/jest-react/src/__test__/matchers.test.ts
+++ app/jest-react/src/__test__/matchers.test.ts
@@ -35,3 +35,52 @@ test('zero', () => {
   expect(z).not.toBeTruthy()
   expect(z).toBeFalsy()
 })
+
+test('two plus two', () => {
+  const value = 2 + 2
+  expect(value).toBeGreaterThan(3)
+  expect(value).toBeGreaterThanOrEqual(3.5)
+  expect(value).toBeLessThan(5)
+  expect(value).toBeLessThanOrEqual(4.5)
+
+  expect(value).toBe(4)
+  expect(value).toEqual(4)
+})
+
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2
+  expect(value).toBeCloseTo(0.3)
+})
+
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/)
+})
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/)
+})
+
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk'
+]
+
+test('the shopping list has milk on it', () => {
+  expect(shoppingList).toContain('milk')
+  expect(new Set(shoppingList)).toContain('milk')
+})
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK')
+}
+
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow()
+  expect(() => compileAndroidCode()).toThrow(Error)
+
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK')
+  expect(() => compileAndroidCode()).toThrow(/JDK/)
+})
