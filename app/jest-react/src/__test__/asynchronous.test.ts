5a5d580bf843945dd00e0f70493236ea4025ed6a
kei-yagi <kei.yagi@gmail.com>
2022-02-24 23:36:03 +0900

    asynchronous

--- app/jest-react/src/__test__/asynchronous.test.ts
+++ app/jest-react/src/__test__/asynchronous.test.ts
@@ -1,12 +1,12 @@
 export {}
 
-const fetchData = (callback: (str: string) => void) => {
-  setTimeout(() => {
-    callback('peanut butter')
-  }, 1000)
-}
-
 test('callbacks - the data is peanut butter', (done) => {
+  const fetchData: (callback: (str: string) => void) => Promise<void> = (callback) => {
+    return new Promise(() => {
+      setTimeout(() => callback('peanut butter'), 1000);
+    })
+  }
+
   function callback(data: string) {
     try {
       expect(data).toBe('peanut butter')
@@ -17,4 +17,61 @@ test('callbacks - the data is peanut butter', (done) => {
   }
 
   fetchData(callback)
+})
+
+describe('promises', () => {
+  const fetchData: () => Promise<string> = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve('peanut butter'), 1000);
+    })
+  }
+
+  test('the data is peanut butter', () => {
+    return fetchData().then(data => {
+      expect(data).toBe('peanut butter')
+    })  
+  })
+
+  test('the fetch fails with an error', () => {
+    expect.assertions(0)
+    return fetchData().catch(e => expect(e).toMatch('error'))
+  })
+
+  test('the data is peanut butter using `.resolves`', () => {
+    return expect(fetchData()).resolves.toBe('peanut butter')
+  })
+
+  test.skip('the fetch fails with an error using `.rejects`', () => {
+    return expect(fetchData()).rejects.toMatch('error')
+  })
+})
+
+describe('async/await', () => {
+  const fetchData: () => Promise<string> = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve('peanut butter'), 1000);
+    })
+  }
+
+  test('the data is peanut butter', async () => {
+    const data = await fetchData()
+    expect(data).toBe('peanut butter')
+  })
+
+  test('the fetch fails with an error', async () => {
+    expect.assertions(0)
+    try {
+      await fetchData()
+    } catch(e) {
+      expect(e).toMatch('error')
+    }
+  })
+
+  test('the data is peanut butter using `.resolves`', () => {
+    return expect(fetchData()).resolves.toBe('peanut butter')
+  })
+
+  test.skip('the fetch fails with an error using `.rejects`', () => {
+    return expect(fetchData()).rejects.toMatch('error')
+  })
 })
